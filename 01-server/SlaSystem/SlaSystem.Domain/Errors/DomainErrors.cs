64aee3c0cdff3ad41b44f5afb3dc0522e71f54cf
Ayman Kheireldeen <aymanmido067@gmail.com>
2023-06-05 10:13:15 +0300

    Adding DomainErrors to Domain Layer

---
+++ 01-server/SlaSystem/SlaSystem.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,12 @@
+﻿using SlaSystem.Domain.Shared;
+
+namespace SlaSystem.Domain.Errors;
+
+public static class DomainErrors
+{
+    public static class Request
+    {
+        public static readonly Error SlaExpired = new("Request.SlaExpired", 
+            "Can't Close this request because the sla has been expired");
+    }
+}
