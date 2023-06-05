571048e1f1d0245f52db7f3b31ad65a9ba064d00
Ayman Kheireldeen <aymanmido067@gmail.com>
2023-06-05 11:07:13 +0300

    Creating Client Request Query

---
+++ 01-server/SlaSystem/SlaSystem.Application/Requests/Queries/GetClientRequestsQuery.cs
@@ -0,0 +1,6 @@
+﻿using SlaSystem.Application.Abstractions.Messaging;
+using SlaSystem.Domain.Entities;
+
+namespace SlaSystem.Application.Requests.Queries;
+
+public record GetClientRequestsQuery(Guid ClientId) : IQuery<List<Request>>;
---
+++ 01-server/SlaSystem/SlaSystem.Application/Requests/Queries/GetClientRequestsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using SlaSystem.Application.Abstractions.Messaging;
+using SlaSystem.Domain.Entities;
+using SlaSystem.Domain.Repositories;
+using SlaSystem.Domain.Shared;
+
+namespace SlaSystem.Application.Requests.Queries;
+
+public class GetClientRequestsQueryHandler : IQueryHandler<GetClientRequestsQuery, List<Request>>
+{
+    private readonly IRequestRepository _requestRepository;
+
+    public GetClientRequestsQueryHandler(IRequestRepository requestRepository) =>
+        _requestRepository = requestRepository;
+    
+    public async Task<Result<List<Request>>> Handle(GetClientRequestsQuery request, CancellationToken cancellationToken)
+    {
+       return await _requestRepository.GetRequestsByClientIdAsync(request.ClientId, 
+            cancellationToken);
+    }
+}
