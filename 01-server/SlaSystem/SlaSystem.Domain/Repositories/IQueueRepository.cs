ce3e0117ea12aec8a44bb934ecb78b5769442251
Ayman Kheireldeen <aymanmido067@gmail.com>
2023-06-05 09:51:40 +0300

    Finishing queues domain model

--- 01-server/SlaSystem/SlaSystem.Domain/Repositories/IQueueRepository.cs
+++ 01-server/SlaSystem/SlaSystem.Domain/Repositories/IQueueRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace SlaSystem.Domain.Repositories;
+﻿using SlaSystem.Domain.Entities;
+using SlaSystem.Domain.Enums;
+
+namespace SlaSystem.Domain.Repositories;
 
 public interface IQueueRepository
 {
-    
+    Task<Queue> CreateQueueAsync(Queue queue);
+    Task<List<Queue>> GetQueuesForRequestType(RequestType requestType);
+    Task<List<Queue>> GetQueues();
 }
